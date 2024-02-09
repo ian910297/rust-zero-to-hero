3e115a100464e20986c9621bba4a23b0e771bee1
Demonic <demonic@csie.io>
2024-02-09 16:36:26 +0800

    Update lesson 2
    1. add loop

--- 2-guessing-game/src/main.rs
+++ 2-guessing-game/src/main.rs
@@ -4,25 +4,31 @@ use rand::Rng;
 
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
+    //println!("The secret number is: {secret_number}");
 
-    println!("Guess the number!");
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("You guessed: {guess}");
-
-    let guess: u32 = guess.trim()
-                        .parse()
-                        .expect("Please input a number");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!")
+    loop {
+        println!("----------------------------");
+        println!("Guess the number!");
+        println!("Please input your guess.");
+    
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        println!("You guessed: {guess}");
+    
+        let guess: u32 = guess.trim()
+                            .parse()
+                            .expect("Please input a number");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # short-url
 
+## rust command
+
+
+## cargo command
+cargo run -> execute application
+cargo init -> initialization for this project
+cargo install [module name] -> download module globally for this machine
+cargo add [module name] -> add module into Cargo.toml for this project
+
+## Others
+* VSCode setting powershell 7 as default
+```batch
+"terminal.integrated.profiles.windows": {
+    "PowerShell_7": {
+      "path": "C:\\Program Files\\PowerShell\\7\\pwsh.exe",
+      "icon": "terminal-powershell"
+    },  // ...
+}
+
+// Make the profile defined above the default profile.
+"terminal.integrated.defaultProfile.windows": "PowerShell_7" 
+```
+
+
